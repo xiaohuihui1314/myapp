24d4af98d36984737606e2d707345a0ce81c9f6f
1243817329@qq.com <520huihui>
2017-02-16 14:00:28 +0800

    --myapp

--- index.android.js
+++ index.android.js
@@ -20,12 +20,9 @@ export default class myapp extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          hello world !
         </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
       </View>
     );
   }
